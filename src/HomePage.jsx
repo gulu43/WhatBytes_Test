8e45f7f1b9773719a0f08274b352ae0bab01fcaf
gulu43 <gulamnabi9th@gmail.com>
2025-08-27 11:01:57 +0530

    added filter and search

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { Product_Context } from './App';
@@ -6,8 +6,19 @@ import './hp.css'
 
 export function HomePage() {
     const product_details = useContext(Product_Context);
-    const [maxPrice, setMaxPrice] = useState(0);
+    const [maxPrice, setMaxPrice] = useState(300);
+    const [categoryRadio, setCategoryRadio] = useState('all');
+    const [inpSearch, setInpSearch] = useState('');
+
+
     // console.log(product_details[15].id);
+
+    useEffect(() => {
+
+        console.log(inpSearch);
+
+    }, [inpSearch])
+
 
     return (
         <>
@@ -18,12 +29,14 @@ export function HomePage() {
                     <img src="./src/assets/logo.png" className="logo" alt="Logo" />
                 </div>
                 <div>
-                    <input type="text" id="search" placeholder='Search for products...' className='inp-search' />
+                    <input type="text" id="search" placeholder='Search for products...' className='inp-search' onChange={(e) => {
+                        setInpSearch(e.target.value)
+                    }} />
                 </div>
                 <div className='cart-div'>
                     <span className='cart-wapper'>
                         <span className='cart-img'>
-                            <img src="./src/assets/cart.png" alt="" />
+                            <img src="./src/assets/cart.png" alt="Cart" />
                         </span>
                         <button className='btn-cart'>Cart</button>
                     </span>
@@ -31,7 +44,7 @@ export function HomePage() {
                 </div>
             </div>
 
-        {/* body */}
+            {/* body */}
             <div className='main-body'>
 
                 <div className='left-main'>
@@ -40,19 +53,27 @@ export function HomePage() {
                         <div className='heading'>Category</div>
                         <div className='radio-labels'>
                             <label>
-                                <input type="radio" name="category" value="All" />
+                                <input type="radio" name="category" value="all" onClick={(e) => {
+                                    setCategoryRadio(e.target.value)
+                                }} defaultChecked />
                                 All
                             </label>
                             <label>
-                                <input type="radio" name="category" value="Electronics" />
+                                <input type="radio" name="category" value="electronics" onClick={(e) => {
+                                    setCategoryRadio(e.target.value)
+                                }} />
                                 Electronics
                             </label>
                             <label>
-                                <input type="radio" name="category" value="Clothing" />
+                                <input type="radio" name="category" value="clothings" onClick={(e) => {
+                                    setCategoryRadio(e.target.value)
+                                }} />
                                 Clothing
                             </label>
                             <label>
-                                <input type="radio" name="category" value="Home" />
+                                <input type="radio" name="category" value="home" onClick={(e) => {
+                                    setCategoryRadio(e.target.value)
+                                }} />
                                 Home
                             </label>
                         </div>
@@ -66,42 +87,76 @@ export function HomePage() {
                 <div className='rigth-main'>
                     <span className='Listing-txt'>{'Product Listing'}</span>
                     <div className='rigth-main-safe-area' >
-                        {product_details.map((item) => (
-                            <div key={item.id} className="card">
-                                <div className="card-image-holder">
-                                    <img src={`/assets/${item.product_image}`} alt={item.product_name} />
-                                </div>
-                                <div className="txt-card">
-                                <div>{item.product_name}</div>
-                                <div>${item.product_price}</div>
-                                <div>Rating {item.product_rating}</div>
-                                </div>
-                                <button>Add to Cart</button>
-                            </div>
+                        {inpSearch != '' ? (
+                            product_details
+                                .filter((item) => (item.product_name.toLowerCase().includes( inpSearch.toLowerCase() ) ))
+                                .map((item) => (
+                                        <div key={item.id} className="card">
 
-                        ))}
+
+                                            <div className="card-image-holder">
+                                                <img src={`/assets/${item.product_image}`} alt={item.product_name} />
+                                            </div>
+                                            <div className="txt-card">
+                                                <div>{item.product_name}</div>
+                                                <div>${item.product_price}</div>
+                                                <div>Rating {item.product_rating}</div>
+                                            </div>
+                                            <button>Add to Cart</button>
+                                        </div>
+
+                                ))
+                            ) :
+                            ( product_details
+                                .filter((item) => {
+                                    // Category filter
+                                    const categoryMatch =
+                                        categoryRadio === "all" || item.product_category === categoryRadio;
+
+                                    // Price filter
+                                    const priceMatch = item.product_price <= maxPrice;
+
+
+                                    return categoryMatch && priceMatch;
+                                })
+                                .map((item) => (
+                                    <div key={item.id} className="card">
+
+
+                                        <div className="card-image-holder">
+                                            <img src={`/assets/${item.product_image}`} alt={item.product_name} />
+                                        </div>
+                                        <div className="txt-card">
+                                            <div>{item.product_name}</div>
+                                            <div>${item.product_price}</div>
+                                            <div>Rating {item.product_rating}</div>
+                                        </div>
+                                        <button>Add to Cart</button>
+                                    </div>
+
+                                )))}
                     </div>
-                    
+
                 </div>
             </div>
 
             <div className="footer">
+                <div>
+                    <div className="heading-footter">Filters</div>
+                    <div>All Items</div>
+                    <div>&copy; 2025 WhatBytes</div>
+                </div>
+                <div>
+                    <div className="heading-footter">About Us</div>
+                    <div>About Us</div>
+                    <div>Contact</div>
+                </div>
+                <div>
+                    <div className="heading-footter">Follow Us</div>
                     <div>
-                        <div className="heading-footter">Filters</div>
-                        <div>All Items</div>
-                        <div>&copy; 2025 WhatBytes</div>
+                        <img src="./src/assets/footer-logo.png" className="footer-img" alt="logo" />
                     </div>
-                    <div>
-                        <div className="heading-footter">About Us</div>
-                        <div>About Us</div>
-                        <div>Contact</div>
-                    </div>
-                    <div>
-                        <div className="heading-footter">Follow Us</div>    
-                        <div>
-                            <img src="./src/assets/footer-logo.png" alt="logo" />
-                        </div>
-                    </div>
+                </div>
             </div>
 
         </>
